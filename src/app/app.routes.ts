fd5e573f52b56e76018515eb920a4231d8d91582
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-10-02 19:46:54 -0300

    feat: add base form layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { QuestionsPage } from './features/questions-form/pages/questions-page/questions-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: QuestionsPage
+  }
+];
---
+++ src/app/features/questions-form/pages/questions-page/questions-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormComponent } from '../../components/form/form';
+
+@Component({
+  selector: 'app-questions-page',
+  imports: [FormComponent],
+  templateUrl: './questions-page.html',
+  styleUrl: './questions-page.css'
+})
+export class QuestionsPage {
+
+}
