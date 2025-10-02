0550f76b647ea57278b83eea02e1c46a3baf04f3
Jorge Miguel Eberhard da Conceição <jorgem.eberhardc@gmail.com>
2025-10-02 19:56:57 -0300

    feat: add answer capturing to form

--- src/app/features/questions-form/components/form/form.ts
+++ src/app/features/questions-form/components/form/form.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.css'
 })
 export class FormComponent {
+  protected form = new FormGroup({
+    question1: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    question2: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    question3: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    question4: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    question5: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    question6: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    question7: new FormControl('', {
+      validators: [Validators.required]
+    })
+  })
 
+  protected onSubmit() {
+    console.log(this.form.value);
+  }
 }
